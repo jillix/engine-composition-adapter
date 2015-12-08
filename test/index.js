5a724d2e5d5113d23ec521d91fc4da96a08d6cea
Ionică Bizău <bizauionica@gmail.com>
2015-12-08 10:02:40 +0200

    Added tests

---
+++ test/index.js
@@ -0,0 +1,38 @@
+const tester = require("tester")
+    , Adapter = require("../lib")
+    ;
+
+tester.describe("run all the adapter functions", test => {
+    var crud = new Adapter()
+      , handler = c => {
+            var name = c[0]
+              , args = c[1]
+              ;
+
+            return cb => {
+                args.push(err => {
+                    test.expect(err).toBeAn(Error);
+                    test.expect(err.code).toBe("METHOD_NOT_IMPLEMENTED");
+                    cb();
+                });
+                crud[name].apply(crud, args);
+            };
+        }
+      , data = [
+            ["exists", ["layout"]]
+          , ["list", []]
+          , ["readAll", []]
+          , ["create", ["layout", { module: "foo" }]]
+          , ["read", ["layout"]]
+          , ["read", ["layout"]]
+          , ["update", ["layout", { module: "foo" }]]
+          , ["remove", ["layout"]]
+          , ["rename", ["layout", "another_layout"]]
+          , ["upsert", ["layout", { module: "foo" }]]
+        ]
+      ;
+
+    data.forEach(c => {
+        test.it(`${c[0]}: should send NOT_IMPLEMENTED_ERR in the response`, handler(c));
+    });
+});
